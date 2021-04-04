69a169b820efea99facd5960fec1040afa75f1f8
bhavyavishal <pilotvsvishal@gmail.com>
2021-04-04 11:37:42 +0530

    start makes game🤗🤗

---
+++ Assets/playercontrol.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{ 
+    public  Rigidbody2D rb;
+        // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(2, rb.velocity.y);
+        
+         if(Input.GetMouseButtonDown(0) ){
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+
+    }
+}
